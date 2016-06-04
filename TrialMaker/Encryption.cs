a68a0424c0f91c5c4d1e65a1bc3a87922bd6e56d
Chaitanya Shete <chshete@microsoft.com>
2016-06-04 16:05:21 -0700

    First commit

--- TrialMaker/Encryption.cs
+++ TrialMaker/Encryption.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 
 namespace SoftwareLocker
 {
+	/* This is Encryption */
     static class Encryption
     {
         static public string InverseByBase(string st, int MoveBase)
